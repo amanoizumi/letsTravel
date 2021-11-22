535f4a6983f7a1baa968a35630ef8b6f5997a288
amanoizumi <a0918233246@gmail.com>
2021-11-22 22:57:45 +0800

    add c3.js

--- javascript/all.js
+++ javascript/all.js
@@ -5,6 +5,7 @@ const ticketCardArea = document.querySelector('.ticketCard-area');
 const searchResultText = document.querySelector('#searchResult-text');
 
 const url = 'https://raw.githubusercontent.com/hexschool/js-training/main/travelApi.json';
+
 let ticketData = [];
 
 function cardTemplate(dataObj) {
@@ -45,6 +46,56 @@ function render(data = ticketData) {
   searchResultText.innerHTML = `本次搜尋共 ${data.length} 筆資料`;
 }
 
+function renderC3() {
+/*
+c3 資料格式：
+[["高雄", 2], ["台北",1], ["台中", 1]]
+*/
+  const obj = {};
+
+  ticketData.forEach((item) => {
+    if (obj[item.area] === undefined) {
+      obj[item.area] = 1;
+    } else {
+      obj[item.area] += 1;
+    }
+  });
+
+  const objKeysArr = Object.keys(obj);
+  const objValuesArr = Object.values(obj);
+
+  const renderC3Arr = [];
+
+  objKeysArr.forEach((item, idx) => {
+    renderC3Arr.push([item, objValuesArr[idx]]);
+  });
+
+  // eslint-disable-next-line no-undef
+  c3.generate({
+    bindto: '.chart',
+    data: {
+      columns: renderC3Arr,
+      type: 'donut',
+      colors: {
+        台北: '#26BFC7',
+        台中: '#5151D3',
+        高雄: '#E68618',
+      },
+    },
+    donut: {
+      title: '套票地區比重',
+      width: 15, // 資料圈圈寬度為%數
+      label: {
+        show: false, // 標籤不顯示
+      },
+    },
+    size: { // 大小
+      height: 200,
+      width: 200,
+    },
+  });
+}
+
 // 事件物件處理
 const eventHandlerObj = {
   addTicket(e) {
@@ -66,7 +117,10 @@ const eventHandlerObj = {
 
     ticketData.push(obj);
     regionSearch.value = '';
+
     render();
+    renderC3();
+
     formEl.reset();
   },
   changeSelect(e) {
@@ -82,13 +136,16 @@ const eventHandlerObj = {
 formEl.addEventListener('submit', eventHandlerObj.addTicket);
 regionSearch.addEventListener('change', eventHandlerObj.changeSelect);
 
-// eslint-disable-next-line no-undef
-axios.get(url).then((res) => {
-  ticketData = res.data.data;
-  render();
-}).catch((err) => {
-  // eslint-disable-next-line no-console
-  console.error(err.status);
-});
+function init() {
+  // eslint-disable-next-line no-undef
+  axios.get(url).then((res) => {
+    ticketData = res.data.data;
+    render();
+    renderC3();
+  }).catch((err) => {
+    // eslint-disable-next-line no-console
+    console.error(err);
+  });
+}
 
-render();
+init();
